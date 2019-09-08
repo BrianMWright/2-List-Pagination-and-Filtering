b00fc271e1d7c186cced674de26c8fb6e0e1ca8e
BrianMWright <45992206+Piixtt@users.noreply.github.com>
2019-09-07 20:14:07 -0500

    Updated JS to display names on pages with pagination placeholders
    Implemented displaying student info on a per page basis and added in pagination buttons (without functionality).

--- js/script.js
+++ js/script.js
@@ -1,24 +1,8 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+//Create variable to store the list of students
+const studentList = document.querySelectorAll('.student-item');
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
-
-
+//Create a variable to store the number of items to show on each “page”, which for this project, is 10.
+const itemsPerPage = 10;
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -35,16 +19,50 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
+/* The list parameter to represent the actual list of students that you’ll pass in as an argument later when you     call this function.
+   
+The page parameter to represent the page number that you’ll pass in as an argument later when you call this function. */
 
+function showPage(list, page) {
+	let startIndex = page * itemsPerPage - itemsPerPage;
+	console.log(`Start Index: ${startIndex}`); // REMOVE ME
+	let endIndex = page * itemsPerPage;
+	console.log(`End Index: ${endIndex}`); // REMOVE ME
 
+	for (let i = 0; i < list.length; i++) {
+		if (i >= startIndex && i < endIndex) {
+			list[i].style.display = 'block';
+		} else {
+			list[i].style.display = 'none';
+		}
+	}
+}
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+function appendPageLinks(list) {
+	//create DOM elements
+	// grab parent node to attach to
+	const divPage = document.querySelector('div.page');
+	//create element
+	const div = document.createElement('div');
+	//add class to element
+	div.className = 'pagination';
+	//append element
+	divPage.append(div);
+	const ul = document.createElement('ul');
+	div.append(ul);
+	//creat the lis
+	const pagesNeeded = Math.ceil(list.length / itemsPerPage);
+	for (i = 0; i < pagesNeeded; i++) {
+		let li = document.createElement('li');
+		let a = document.createElement('a');
+		a.setAttribute('href', '#');
+		a.textContent = `${i + 1}`; //need to get the page number somehow
+		ul.append(li);
+		li.append(a);
+		console.log(a);
+	}
+	// functionality for the pagination elements
+}
 
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+showPage(studentList, 1); //Remove hard coded page number.
+appendPageLinks(studentList);
